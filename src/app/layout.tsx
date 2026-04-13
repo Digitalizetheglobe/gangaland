aee3604b3410b57c20650f4839447f6389078de7
Your Name <your.email@example.com>
2026-04-13 17:00:53 +0530

    added metas in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono, Playfair_Display, Raleway } from "next/font/google";
 import "./globals.css";
 
@@ -23,8 +24,12 @@ const raleway = Raleway({
 });
 
 export const metadata: Metadata = {
-  title: "Ganga Legend",
-  description: "Ganga Legend is a residential township located in Pune, Maharashtra. It offers a range of amenities and facilities for its residents.",
+  title: "The Living Blueprint | 3 & 3.5 BHK Sports First Township in Bavdhan, Pune",
+  description: "Explore the Living Blueprint, a sports-first township in Bavdhan, Pune. Buy premium 3 BHK & 3.5 BHK homes with world-class sports academies & amenities.",
+  keywords: "3 BHK flats in Bavdhan Pune, 3.5 BHK apartments in Bavdhan, Luxury homes in Bavdhan Pune, new residential projects in Bavdhan, residential township, premium residencies, luxury amenities, Sports township Pune, luxury living, luxury township pune, ganga legend county, Ganga Legends County Bavdhan, the living blueprint bavdhan",
+  verification: {
+    google: "k55m18oJDAzXQXcks9kGQeR7bZLiU4yIwwn7IUG2H-s",
+  },
 };
 
 import FixedButtons from "@/components/FixedButtons";
@@ -40,6 +45,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} ${raleway.variable} antialiased`}
       >
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HJCY0G2HZ3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HJCY0G2HZ3');
+          `}
+        </Script>
         <ScheduleModalProvider>
           {children}
           <FixedButtons />
